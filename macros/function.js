708995aef29be17af0b62686ac267555c0fef2c6
Philip Weaver <simplygreatwork@gmail.com>
2020-03-10 22:08:44 -0500

    shove

--- macros/function.js
+++ macros/function.js
@@ -14,8 +14,8 @@ module.exports = function(system, document) {
 			if (query.is_type_value(node.value[0], 'symbol', 'function')) node.value[0].value = 'func'
 			if (! query.is_type_value(node.value[0], 'symbol', 'func')) return
 			if (! query.is_expression_longer(node, 1)) return
+			if (! query.is_type(node.value[1], 'symbol')) return
 			if (! node.value[1].value) return
-			if (! (node.value[1].type == 'symbol')) return
 			state.func = node
 			state.locals = shared.find_locals(state)
 			node.value[1].value = shared.dollarify(node.value[1].value)
@@ -26,8 +26,8 @@ module.exports = function(system, document) {
 			if (! query.is_type(node, 'expression')) return
 			if (! query.is_type_value(node.value[0], 'symbol', 'func')) return
 			if (! query.is_expression_longer(node, 1)) return
+			if (! query.is_type(node.value[1], 'symbol')) return
 			if (! node.value[1].value) return
-			if (! (node.value[1].type == 'symbol')) return
 			state.func = null
 		}
 	}
