a00eaf432f41c8567b49f16ed2f6abed4f4cb82c
Philip Weaver <simplygreatwork@gmail.com>
2020-03-24 03:41:46 -0500

    shove

--- compiler/emitter.js
+++ compiler/emitter.js
@@ -3,10 +3,11 @@ function on(key, func) {
 	
 	this.channels = this.channels || {}
 	if (this.channels[key] === undefined) this.channels[key] = []
-	this.channels[key].push(func)
+	let channel = this.channels[key]
+	channel.push(func)
 	return function() {
-		let index = this.channels[key].indexOf(func)
-		this.channels[key].splice(index, 1)
+		let index = channel.indexOf(func)
+		channel.splice(index, 1)
 	}.bind(this)
 }
 
@@ -21,10 +22,11 @@ function once(key, func) {
 function emit(key) {
 	
 	this.channels = this.channels || {}
-	if (this.channels[key]) {
-		for (let i = 0; i < this.channels[key].length; i++) {
-			let index = this.channels[key].length - i - 1
-			result = this.channels[key][index].apply(this, Array.from(arguments).splice(1))
+	let channel = this.channels[key]
+	if (channel) {
+		for (let i = 0; i < channel.length; i++) {
+			let index = channel.length - i - 1			// reverse order yet permitting growth of length (still review)
+			result = channel[index].apply(this, Array.from(arguments).splice(1))
 			if (result == false) return false
 		}
 	}
