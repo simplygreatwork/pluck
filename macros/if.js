cf514d57c1ea49c6b655e62f0fded590d2855800
Philip Weaver <simplygreatwork@gmail.com>
2020-03-24 19:25:06 -0500

    shove

--- macros/if.js
+++ macros/if.js
@@ -3,9 +3,6 @@ const query = require('../compiler/query')
 const parse = require('../compiler/parse')
 const shared = require('./shared')
 
-// goal: no keyword "then": all expressions inside of "if" but after (if (cond)) are shifted into (if (then))
-// goal: keyword "else" is a sibling of "if" - else contents are moved into (if (else))
-
 module.exports = function(system, document) {
 	
 	return {
