6ef2673cc02c0d20f122c7b1fef5106b24349fd7
Philip Weaver <simplygreatwork@gmail.com>
2020-03-30 00:46:52 -0500

    shove

--- runtime/thread.js
+++ runtime/thread.js
@@ -7,8 +7,16 @@ onmessage = function (event) {
 		properties[event.data.key] = event.data.value
 		postMessage({ action: 'message', message: `Worker key "${event.data.key}" has been set to "${event.data.value}".` })
 	} else if (event.data.action == 'start') {
+		let id = setInterval(loop, 1)
+		setTimeout(function() {
+			clearInterval(id)
+			postMessage({ action: 'message', message: 'Worker has been terminated.' })
+			postMessage({ action: 'terminate' })
+		}, 1000)
 		postMessage({ action: 'message', message: 'Worker has been started.' })
-		postMessage({ action: 'message', message: 'Worker has been terminated.' })
-		postMessage({ action: 'terminate' })
 	}
+}
+
+function loop() {
+	console.log('thread: ' + properties.name)
 }
