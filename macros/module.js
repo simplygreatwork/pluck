bc87e77811a910128dc3f44245260d3923d4b258
Philip Weaver <simplygreatwork@gmail.com>
2020-04-14 04:03:46 -0500

    shove

---
+++ macros/module.js
@@ -0,0 +1,19 @@
+
+const query = require('../compiler/query')
+const parse = require('../compiler/parse')
+
+module.exports = function(system, document) {
+	
+	return {
+		
+		type: 'symbol',
+		value: 'module',
+		
+		enter : function(node, index, parents, state) {
+			
+			let parent = query.last(parents)
+			if (! query.is_type(parent, 'expression')) return
+			if (! query.is_type_value(node, 'symbol', 'import')) return
+		}
+	}
+}
